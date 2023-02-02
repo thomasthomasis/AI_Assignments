a853fdf139ad66c6bbbdf2fa27a2b255691a5da4
Thomas Sloane <thomas.i.sloane@gmail.com>
2023-02-02 01:34:20 +0000

    work done

---
+++ partB/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+
+// Hello World! program
+namespace PartB
+{
+
+    class Hello 
+    {         
+        static void Main(string[] args)
+        {
+            ArrayList lecturers = new ArrayList();
+            ArrayList students = new ArrayList();
+            ArrayList capacities = new ArrayList();
+            ArrayList preferences = new ArrayList();
+
+            lecturers.Add("lecturer1");
+            lecturers.Add("lecturer2");
+            lecturers.Add("lecturer3");
+            lecturers.Add("lecturer4");
+            lecturers.Add("lecturer5");
+
+            capacities.Add(1);
+            capacities.Add(1);
+            capacities.Add(1);
+            capacities.Add(1);
+            capacities.Add(1);
+
+            students.Add("student1");
+            students.Add("student2");
+            students.Add("student3");
+            students.Add("student4");
+            students.Add("student5");
+
+            preferences.Add(new int[] = {1, 2, 3, 4, 5});
+
+
+        }
+
+    }
+
+}
